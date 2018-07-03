8fe37e0576b2dd4fc564adff9d6e67fcacf912c7
Naresh Thakur <thakurinbox@gmail.com>
2018-07-03 09:57:48 +0530

    feat: removed yarn lock and added header compnent

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-/* import React from 'react';
- * import ReactDOM from 'react-dom';
- * import App from './views/App';
- * 
- * ReactDOM.render(<App />, document.getElementById('root'));*/
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -14,7 +6,7 @@ import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import configureStore from './configureStore';
 
-import  App  from './views/App/index.js';
+import  App  from './views/App';
 
 import { attemptRelogin } from './ducks';
 
